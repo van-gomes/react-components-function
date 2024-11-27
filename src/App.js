28ba9cbb97385f38fd5f80f8a04ae483b8f2b263
van-gomes <nninha.gomes@gmail.com>
2024-11-27 15:38:00 -0300

    Feature change time color completed

--- src/App.js
+++ src/App.js
@@ -10,44 +10,37 @@ function App() {
     {
       id: 1,
       nome: 'Programação',
-      corPrimaria: '#D9F7E9',
-      corSecundaria: '#57C278'
+      cor: '#57C278'
     },
     {
       id: 2,
       nome: 'Front-End',
-      corPrimaria: '#E8F8FF',
-      corSecundaria: '#82CFFA'
+      cor: '#82CFFA'
     },
     {
       id: 3,
       nome: 'Data Science',
-      corPrimaria: '#F0F8E2',
-      corSecundaria: '#A6D157'
+      cor: '#A6D157'
     },
     {
       id: 4,
       nome: 'Devops',
-      corPrimaria: '#FDE7E8',
-      corSecundaria: '#E06B69'
+      cor: '#E06B69'
     },
     {
       id: 5,
       nome: 'UX e Design',
-      corPrimaria: '#FAE9F5',
-      corSecundaria: '#DB6EBF'
+      cor: '#DB6EBF'
     },
     {
       id: 6,
       nome: 'Mobile',
-      corPrimaria: '#FFF5D9',
-      corSecundaria: '#FFBA05'
+      cor: '#FFBA05'
     },
     {
       id: 7,
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FFEEDF',
-      corSecundaria: '#FF8A29'
+      cor: '#FF8A29'
     },
   ])
 
@@ -231,7 +224,7 @@ function App() {
   function mudarCorDoTime(cor, id) {
     setTimes(times.map(time => {
         if(time.id=== id) {
-            time.corSecundaria = cor;
+            time.cor = cor;
         }
         return time;
     }));
